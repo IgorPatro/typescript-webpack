5f313f3fd5100d9076c123545a8ef9d00e1ae210
Igor Patro <i.patro@wp.pl>
2022-04-02 17:50:34 +0200

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  mode: process.env.ENV ?? "production",
+}
